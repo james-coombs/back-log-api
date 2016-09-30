3c63681fd20685aaf1133ff15c18672680585107
james-coombs <coombsj4@yahoo.com>
2016-09-30 17:06:38 -0400

    Add passed column to logs
    Create passed column (boolean, null:false, default:false) in logs table
    via rails migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930204332) do
+ActiveRecord::Schema.define(version: 20160930204940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,8 +31,9 @@ ActiveRecord::Schema.define(version: 20160930204332) do
     t.string   "players"
     t.string   "stage"
     t.string   "notes"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "passed",     default: false, null: false
   end
 
   create_table "users", force: :cascade do |t|
