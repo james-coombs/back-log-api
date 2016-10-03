a77532606bd9d306999a6c2b5d6e8ad88c17c7e8
james-coombs <coombsj4@yahoo.com>
2016-10-03 11:14:18 -0400

    Remove username
    Remove username column from users via rails migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930211156) do
+ActiveRecord::Schema.define(version: 20161003151251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,6 @@ ActiveRecord::Schema.define(version: 20160930211156) do
     t.string   "password_digest", null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
