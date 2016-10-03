ec1bb329b127836a57bf9e5b433ec4f920338628
james-coombs <coombsj4@yahoo.com>
2016-10-03 11:07:47 -0400

    Modify sign in script
    Modify sign in script to test user sign up from terminal client
    to deployed api.

--- scripts/sign-up.sh
+++ scripts/sign-up.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-curl --include --request POST http://localhost:3000/sign-up \
+curl --include --request POST https://back-log.herokuapp.com/sign-up \
   --header "Content-Type: application/json" \
   --data '{
     "credentials": {
-      "email": "an@example.email",
-      "password": "an example password",
+      "username": example2
+      "email": "an@example2.email",
+      "password": "an example2 password2",
       "password_confirmation": "an example password"
     }
   }'
