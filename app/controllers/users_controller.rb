94cb4e5090f328213313a1d61dd286080ea7a0b9
james-coombs <coombsj4@yahoo.com>
2016-10-03 10:46:07 -0400

    Add username to users params
    Add username to permitted users creation params in users  controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,7 +60,7 @@ class UsersController < ProtectedController
 
   def user_creds
     params.require(:credentials)
-          .permit(:email, :password, :password_confirmation)
+          .permit(:email, :password, :password_confirmation, :username)
   end
 
   def pw_creds
