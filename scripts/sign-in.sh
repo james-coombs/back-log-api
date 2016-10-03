0617b7fa8b440922b27f6bb9dc08391a9b0b5b2c
james-coombs <coombsj4@yahoo.com>
2016-10-03 13:31:28 -0400

    Modify scripts
    Modify sign in scripts

--- scripts/sign-in.sh
+++ scripts/sign-in.sh
@@ -8,3 +8,13 @@ curl --include --request POST http://localhost:3000/sign-in \
       "password": "an example password"
     }
   }'
+
+
+  curl --include --request POST https://back-log.herokuapp.com/sign-in \
+    --header "Content-Type: application/json" \
+    --data '{
+      "credentials": {
+        "email": "an@example.email",
+        "password": "an example password"
+      }
+    }'
